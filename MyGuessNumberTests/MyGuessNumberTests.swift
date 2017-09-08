02a17bf6273bc34fc9da79a7eae13453703920c5
sample <pandddust@gmail.com>
2017-09-08 13:35:51 +0800

    Initial Commit

---
+++ MyGuessNumberTests/MyGuessNumberTests.swift
@@ -0,0 +1,36 @@
+//
+//  MyGuessNumberTests.swift
+//  MyGuessNumberTests
+//
+//  Created by sample on 2017/9/8.
+//  Copyright © 2017年 sample. All rights reserved.
+//
+
+import XCTest
+@testable import MyGuessNumber
+
+class MyGuessNumberTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
